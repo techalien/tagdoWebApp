43155bb082a69585de25a8a20e71523b88782901
Karan Sharma <karansharma1295@gmail.com>
2015-11-26 11:41:09 +0530

    signup

--- script.js
+++ script.js
@@ -26,6 +26,10 @@
 				templateUrl : 'pages/login.html',
 				controller  : 'loginController'
 			})
+        .when('/signup', {
+				templateUrl : 'pages/signup.html',
+				controller  : 'signupController'
+			})
 			.when('/logout', {
 				templateUrl : 'pages/logout.html',
 				controller  : 'logoutController'
@@ -151,7 +155,33 @@
 			});
 		};
 	});
-
+scotchApp.controller('signupCtrl', function($scope, $alert, $auth) {
+    $scope.signup = function() {
+      $auth.signup({
+        displayName: $scope.displayName,
+        email: $scope.email,
+        password: $scope.password,
+      }).catch(function(response) {
+        if (typeof response.data.message === 'object') {
+          angular.forEach(response.data.message, function(message) {
+            $alert({
+              content: message[0],
+              animation: 'fadeZoomFadeDown',
+              type: 'info',
+              duration: 3
+            });
+          });
+        } else {
+          $alert({
+            content: response.data.message,
+            animation: 'fadeZoomFadeDown',
+            type: 'info',
+            duration: 3
+          });
+        }
+      });
+    };
+  });
 scotchApp.controller('TodoCtrl',function ($scope) {
   
   $scope.todos = [
